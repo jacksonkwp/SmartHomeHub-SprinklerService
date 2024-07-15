3c2fb90177b629dfb14abe8555b529550df982b2
jacksonkwp <jackson.k.w.p@gmail.com>
2024-07-15 15:46:05 -0700

    enabled discovery client

--- SprinklerService/src/main/java/edu/baylor/sprinklerservice/SprinklerServiceApplication.java
+++ SprinklerService/src/main/java/edu/baylor/sprinklerservice/SprinklerServiceApplication.java
@@ -2,8 +2,10 @@ package edu.baylor.sprinklerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SprinklerServiceApplication {
 
 	public static void main(String[] args) {
